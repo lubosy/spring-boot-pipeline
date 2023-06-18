8053e65d9e7aeb8a5a70bd8f23cbe9a7d7e8f187
Lubosy <amatendelubosi@gmail.com>
2023-06-18 19:49:07 +0200

    Commit 2

--- src/main/java/com/pipeline/example/demo/TestController.java
+++ src/main/java/com/pipeline/example/demo/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     // demo git actions project
-
+    // test endpoint
     @GetMapping("getstuff")
     public ResponseEntity<?> getStatus(){
 
